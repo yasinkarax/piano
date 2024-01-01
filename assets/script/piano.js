a68bb1604e622fecacc4b7d4fee4b368aa4297cb
yasinkarax <yasinkara99@hotmail.com>
2024-01-01 16:31:45 +0300

    keyboard letters of each key added

--- assets/script/piano.js
+++ assets/script/piano.js
@@ -1,6 +1,9 @@
 const keys = document.querySelectorAll('.piano button')
 const keysL = keys.length;
 const audios = document.getElementsByTagName('audio');
+const wKeys = document.querySelectorAll('.white-button');
+const bKeys = document.querySelectorAll('.black-button');
+// wKeys[0].innerHTML = "A"
 
 for(let i = 0; i <= keysL; i+=1){
     keys[i].addEventListener('click', () => {
