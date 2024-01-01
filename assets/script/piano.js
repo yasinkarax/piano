f62d7121f2bc522cf4f8b3b8d452c941b844e258
yasinkarax <yasinkara99@hotmail.com>
2024-01-01 19:26:46 +0300

    unneccesary variables deleted

--- assets/script/piano.js
+++ assets/script/piano.js
@@ -1,8 +1,6 @@
 const keys = document.querySelectorAll('.piano button')
 const keysL = keys.length;
 const audios = document.getElementsByTagName('audio');
-const wKeys = document.querySelectorAll('.white-button');
-const bKeys = document.querySelectorAll('.black-button');
 // wKeys[0].innerHTML = "A"
 
 for(let i = 0; i <= keysL; i+=1){
