e3e975d1eb71d772d1f3b3d1a529bab845d35533
yasinkarax <yasinkara99@hotmail.com>
2024-01-01 19:19:38 +0300

    the piano can be play using keyboard

--- assets/script/piano.js
+++ assets/script/piano.js
@@ -1,13 +1,19 @@
 const keys = document.querySelectorAll('.piano button')
 const keysL = keys.length;
 const audios = document.getElementsByTagName('audio');
-const wKeys = document.querySelectorAll('.white-button');
-const bKeys = document.querySelectorAll('.black-button');
-// wKeys[0].innerHTML = "A"
 
-for(let i = 0; i <= keysL; i+=1){
+// listen keydown events
+document.addEventListener('keydown', (e) => {
+    const keyV = e.key.toUpperCase();
+    audios[keyV].play();
+    audios[keyV].currentTime = 0;
+})
+
+
+for (let i = 0; i <= keysL; i += 1) {
     keys[i].addEventListener('click', () => {
         audios[i].currentTime = 0;
         audios[i].play();
     })
 }
+
